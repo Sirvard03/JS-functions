7a8f0101bc5dc7f5874799c2af386c73c2993519
Sirvard03 <111896504+Sirvard03@users.noreply.github.com>
2022-09-09 10:18:44 +0400

    Add files via upload
    add function for prime numbers
    change res.end('isprime!')

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.end('isPrime!');
 }).listen(8080);
+function isPrime(n)
+{
+  if (n<=1)
+  { return 0
+
+  }
+  else if (n==2)
+  {
+    return 1
+  }else
+  {
+    for (var i=2; i<n; i++)
+    {
+      if(n%i==0)
+      {
+        return 0
+      }
+    }
+    return 1;
+  }
+}
